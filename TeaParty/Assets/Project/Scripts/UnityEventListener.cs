7c4a5b5328b59d6df35a1e93f9fedd321a26b1dc
Anthony Bogli <anthonybogli1@gmail.com>
2016-09-23 17:59:40 +1000

    Commiting tea party game
    Tea party gaming

---
+++ TeaParty/Assets/Project/Scripts/UnityEventListener.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class UnityEventListener : MonoBehaviour
+{
+    [SerializeField]
+    UnityEventScript _scriptReference;
+
+    void OnEnable()
+    {
+        if (_scriptReference != null)
+        {
+            //_scriptReference.onStart.AddListener(OnStart);
+
+            //_scriptReference.onStart += OnStart;
+        }
+    }
+
+    void OnDisable()
+    {
+        if (_scriptReference != null)
+        {
+            //_scriptReference.onStart.RemoveListener(OnStart);
+
+            //_scriptReference.onStart -= OnStart;
+        }
+    }
+
+    void OnStart()
+    {
+        Debug.Log("I did the thing");
+    }
+}
---
+++ TeaParty/Assets/Project/Scripts/UnityEventScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System;
+
+public class UnityEventScript : MonoBehaviour
+{
+    [SerializeField]
+    Action _onStart;
+    
+    public Action onStart
+    {
+        get
+        {
+            return _onStart;
+        }
+
+        set
+        {
+            _onStart = value;
+        }
+    }
+
+    void Start()
+    {
+        _onStart.Invoke();
+    }
+}
